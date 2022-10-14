693c50c6b48cbd60f2789342030f801382ed8afa
Tomasz Sulima <tsulima1@stu.vistula.edu.pl>
2022-10-14 11:03:04 +0200

    second commit

--- src/pl/vistula/helloworldsulima/HelloWorldSulima.java
+++ src/pl/vistula/helloworldsulima/HelloWorldSulima.java
@@ -3,5 +3,6 @@ package pl.vistula.helloworldsulima;
 public class HelloWorldSulima {
     public static void main(String[] args) {
         System.out.println("Hello Vistula Sulima");
+        System.out.println("Hello Vistula Sulima");
     }
 }
